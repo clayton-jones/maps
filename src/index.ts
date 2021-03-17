504c028a8debde11107674ea02360a99802678df
Clayton Jones <claytonjonesLMP@gmail.com>
2021-03-17 13:10:50 -0700

    added google map functionality

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hi');
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+console.log(user);
+
+const company = new Company();
+console.log(company);
+
+// new google.maps.Map(document.getElementById('map'), {
+//   zoom: 1,
+//   zoomControl: true,
+//   center: {
+//     lat: 0,
+//     lng: 0,
+//   },
+// });
